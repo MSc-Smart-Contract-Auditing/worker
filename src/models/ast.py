715b3b80de8f52b144508a6f031ffd948991588b
Kristian-A <kristian.zh.apostolov@gmail.com>
2024-08-03 20:04:20 +0100

    Link by ids, instead of function names

--- src/models/ast.py
+++ src/models/ast.py
@@ -8,20 +8,12 @@ class Contract(BaseModel):
     raw: str
 
 
-class FunctionSignature(BaseModel):
-    contract: str
+class Function(BaseModel):
+    id: int
     name: str
-
-
-class Function(FunctionSignature):
     source: str
-
-
-class FunctionNodeAbstract(BaseModel):
-    node: Function
-    invocations: List[FunctionSignature]
 
 
 class FunctionNode(BaseModel):
     node: Function
-    children: List[Function]
+    invocations: List[int]
