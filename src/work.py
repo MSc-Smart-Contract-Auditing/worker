a443de5b23650b17f45b1b9bd582137d75d964d1
Kristian-A <kristian.zh.apostolov@gmail.com>
2024-08-18 20:39:08 +0100

    WIP

--- src/work.py
+++ src/work.py
@@ -8,32 +8,15 @@ from src.smart_contracts.dependency_tree import DependencyTree
 from src.analysis.model import MODEL
 
 
-def get_dependency_tree(data: WorkUnit):
-    return DependencyTree(data)
-
-
 # TODO: If the UI never connects persist the result for sometime
 # and return it when the UI tries to connect
 async def process_work(work: WorkUnit):
     await SocketService.send({"status": "building_dt"})
 
-    dt = get_dependency_tree(work)
-
     dt = DependencyTree(work)
 
-    for item in dt.lookup.items():
-        print(item)
-
-    print()
-    print()
-
-    ids = dt.get_main_ids()
-
-    result = dt.tree(ids[0])
-
-    print(result["main"])
-    for dependency in result["dependencies"]:
-        print(dependency)
+    for id in dt.get_main_ids():
+        print(dt.tree(id))
 
     steps = 5
     for i in range(steps):
